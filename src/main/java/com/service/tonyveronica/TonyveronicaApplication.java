02c33f4e5045b27daa7a1d38badba0b97ec8c145
KANGYUSEOK <111121447+kangyuseok@users.noreply.github.com>
2024-07-01 20:53:13 +0900

    first commit

---
+++ src/main/java/com/service/tonyveronica/TonyveronicaApplication.java
@@ -0,0 +1,13 @@
+package com.service.tonyveronica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TonyveronicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TonyveronicaApplication.class, args);
+	}
+
+}
