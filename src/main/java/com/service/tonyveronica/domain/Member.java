104a40519a27f30e96de2dd09e844ba956d41eb2
강유석 <gang-yuseog@gang-yuseog-ui-MacBookPro.local>
2024-07-04 02:50:49 +0900

    feat: member에 jpa 설정

--- src/main/java/com/service/tonyveronica/domain/Member.java
+++ src/main/java/com/service/tonyveronica/domain/Member.java
@@ -1,16 +1,35 @@
 package com.service.tonyveronica.domain;
 
+import jakarta.persistence.*;
+import lombok.Generated;
 import lombok.Getter;
 import lombok.Setter;
-
+@Entity
+@Table(name="users")
 @Setter
 @Getter
 public class Member {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY) //기본키 자동 생성
+    private Long id;
+
+    @Column(name="email", nullable = false)
     private String email;
+
+    @Column(name="password", nullable = false)
     private String password;
+
+    @Column(name="nickname", nullable = false)
     private String nickName;
+
+    @Column(name="user_image", nullable = false)
+    private String imagePath;
+
+    @Column(name="delete", nullable = false)
     private boolean isDelete; //삭제 여부
 
+    public Member() {
+    }
 
     public Member(String email, String password, String nickName, boolean isDelete) {
         this.email = email;
