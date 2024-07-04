ec1f36ba2e50a81326a95f92bb864e68134bcb25
강유석 <gang-yuseog@gang-yuseog-ui-MacBookPro.local>
2024-07-05 02:29:09 +0900

    feat: service레이어 email, nickname 중복 체크 메서드 구현

--- src/main/java/com/service/tonyveronica/service/MemberService.java
+++ src/main/java/com/service/tonyveronica/service/MemberService.java
@@ -2,6 +2,17 @@ package com.service.tonyveronica.service;
 
 import com.service.tonyveronica.domain.Member;
 
+import java.util.List;
+
 public interface MemberService {
-    public int joinMember(Member member);
+
+
+    Long join(Member member);
+
+    List<Member> findMembers();
+
+    Member isDuplicateEmail(String email);
+
+    Member isDuplicateNickname(String nickName);
+
 }
