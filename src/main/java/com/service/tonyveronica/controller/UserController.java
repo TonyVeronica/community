e5e567f265051a06231c7ed40c85c48da1083093
강유석 <gang-yuseog@gang-yuseog-ui-MacBookPro.local>
2024-07-09 01:06:47 +0900

    feat: 회원정보 수정, 비밀 번호 수정 컨트롤러 생성

--- src/main/java/com/service/tonyveronica/controller/UserController.java
+++ src/main/java/com/service/tonyveronica/controller/UserController.java
@@ -1,20 +1,32 @@
 package com.service.tonyveronica.controller;
 
 import com.service.tonyveronica.domain.Member;
+import com.service.tonyveronica.dto.CustomMemberDetails;
 import com.service.tonyveronica.dto.JoinDTO;
 import com.service.tonyveronica.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Base64;
 import java.util.HashMap;
+import java.util.Map;
 
 @CrossOrigin(origins = {"http://localhost:5173", "http://localhost:8080"}, allowCredentials = "true")
 @RequiredArgsConstructor
@@ -77,7 +89,7 @@ public class UserController {
         else return new ResponseEntity(member, HttpStatus.OK);
     }
 
-    @PostMapping("users/nickname") //이메일 중복 확인
+    @PostMapping("users/nickname") //닉네임 중복 확인
     public ResponseEntity isDuplicateNickname(@RequestBody HashMap<String, Object>requestJsonHashMap) {
         System.out.println(requestJsonHashMap);
         String nickname = (String) requestJsonHashMap.get("nickname");
@@ -91,4 +103,92 @@ public class UserController {
         } else return new ResponseEntity(member, HttpStatus.OK);
     }
 
+    @GetMapping("/users/user")
+    public ResponseEntity viewMemberInfo(Authentication authentication){
+        // 인증된 사용자의 정보를 가져옵니다.
+        CustomMemberDetails customMemberDetails = (CustomMemberDetails) authentication.getPrincipal();
+        String username = customMemberDetails.getUsername();
+        System.out.println("username = " + username);
+
+        Member member = memberService.isDuplicateEmail(username);
+
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("email", member.getEmail());
+        response.put("nickname", member.getNickName());
+        response.put("imagePath", member.getImagePath());
+        return ResponseEntity.ok(response);
+    }
+    @GetMapping("/users/user/image") //이미지 조회
+    public ResponseEntity<Resource> getUserImage(@RequestParam String imagePath) {
+        System.out.println("이미지 경로 = " + imagePath);
+        File file = new File(imagePath);
+        if (file.exists()) {
+            Resource resource = new FileSystemResource(file);
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getName() + "\"")
+                    .contentType(MediaType.parseMediaType("image/png"))
+                    .body(resource);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+
+    @PatchMapping("/users/user/profile")
+    public ResponseEntity updateUserInfo(@RequestBody HashMap<String, Object>requestJsonHashMap, Authentication authentication){
+        System.out.println("회원 정보 업데이트 시작");
+
+        CustomMemberDetails customMemberDetails = (CustomMemberDetails) authentication.getPrincipal();
+        String email = customMemberDetails.getUsername();
+
+        String profileImageBase64 = (String) requestJsonHashMap.get("profileImage");
+
+        //이미지가 null일 경우 다시 처리해야 함
+
+        if (profileImageBase64 != null && !profileImageBase64.isEmpty()) {
+            // Base64 헤더 제거
+            String base64Image = profileImageBase64.split(",")[1];
+            try {
+                // Base64 디코딩
+                byte[] imageBytes = Base64.getDecoder().decode(base64Image);
+                System.out.println(imageBytes);
+
+                // 파일 저장
+                File file = new File("image/"+email+".png");
+                try (OutputStream os = new FileOutputStream(file)) {
+                    os.write(imageBytes);
+                }
+
+                System.out.println("이미지 저장 성공: " + file.getAbsolutePath());
+
+                System.out.println("email = " + email);
+
+                String nickname = (String)requestJsonHashMap.get("nickname");
+                System.out.println("nickname = " + nickname);
+
+                int isSuccess = memberService.updateNickname(nickname, email);
+                System.out.println(isSuccess);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
+        System.out.println("성공");
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @PatchMapping("users/user/password")
+    public ResponseEntity updateUserPassword(@RequestBody HashMap<String, Object>requestJsonHashMap, Authentication authentication){
+        System.out.println("비밀 번호 수정!!!");
+
+        CustomMemberDetails customMemberDetails = (CustomMemberDetails) authentication.getPrincipal();
+        String email = customMemberDetails.getUsername();
+        String password = (String)requestJsonHashMap.get("password");
+
+        int isSuccess = memberService.updatePassword(password, email);
+        System.out.println("isSuccess = " + isSuccess);
+
+        return new ResponseEntity(HttpStatus.OK);
+    }
 }
