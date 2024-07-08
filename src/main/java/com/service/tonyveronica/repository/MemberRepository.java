dac38091602811db19d2ac3ace41e41cdae35601
강유석 <gang-yuseog@gang-yuseog-ui-MacBookPro.local>
2024-07-09 01:10:17 +0900

    feat: 회원정보 수정, 비밀번호 수정 기능 구현

--- src/main/java/com/service/tonyveronica/repository/MemberRepository.java
+++ src/main/java/com/service/tonyveronica/repository/MemberRepository.java
@@ -2,8 +2,10 @@ package com.service.tonyveronica.repository;
 
 import com.service.tonyveronica.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.parameters.P;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     @Query("select m  from Member m where m.email = :email")
@@ -11,5 +13,13 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @Query("select m from Member m where m.nickName = :nickname")
     Member existsByNickname(@Param("nickname") String nickname);
+
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE Member m SET m.nickName = :nickname where m.email = :email")
+    int updateMemberByNickName(@Param("nickname")String nickname, @Param("email")String email);
+
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE Member m SET m.password = :password where m.email = :email")
+    int updateMemberByPassword(@Param("password")String password, @Param("email")String email);
 
 }
