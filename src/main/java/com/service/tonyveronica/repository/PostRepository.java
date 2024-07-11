0714cd71a1070a59bc5e25328b746506f7daea90
강유석 <gang-yuseog@gang-yuseog-ui-MacBookPro.local>
2024-07-11 15:31:59 +0900

    feat: 댓글수 세는 sql 추가

--- src/main/java/com/service/tonyveronica/repository/PostRepository.java
+++ src/main/java/com/service/tonyveronica/repository/PostRepository.java
@@ -2,7 +2,11 @@ package com.service.tonyveronica.repository;
 
 import com.service.tonyveronica.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+    @Query("SELECT COUNT(c) FROM Comment c WHERE c.postId = :postId")
+    Long countCommentsByPostId(@Param("postId") Long postId);
 }
--- src/main/java/com/service/tonyveronica/service/PostService.java
+++ src/main/java/com/service/tonyveronica/service/PostService.java
@@ -4,9 +4,14 @@ import com.service.tonyveronica.domain.Post;
 import com.service.tonyveronica.dto.PostCreateDTO;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PostService {
     Long createPost(PostCreateDTO postCreateDTO, String email);
 
     List<Post> getAllPosts();
+
+    Optional<Post> getOnePost(String postId);
+
+    Long countComments(Long postId);
 }
